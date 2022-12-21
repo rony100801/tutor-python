3b20548f49ffd0b08f762e27e1cf2b33997b2a99
rony100801 <setiawanrony603@gmail.com>
2022-12-21 08:12:48 +0000

    Belajar pengenalan string

---
+++ 15 - Pengenalan String/main.py
@@ -0,0 +1,57 @@
+# Pengenalan String
+
+print("\n")
+data = "Ini adalah string"
+print(data)
+
+# 1. Dapat menggunakan single quote
+data = 'Ini adalah string single quote'
+print(data)
+
+# 2. Dapat menggunakan double quote
+data = "Ini adalah string double quote"
+print(data)
+
+# 3. Bisa digabungkan single quoute dan double quote
+data = "Hari ini adalah hari jum'at"
+print(data)
+
+# 4. Menggunakan tanda backslash \
+data = 'Mari shoalt jum\'at'
+print(data)
+print('g\'day, isn\'t it')
+
+# 5. Backslash
+print("C:\\User\\Rony\\AppData\\Roaming")
+
+# 6. Tab
+print("Rony \t Setiawan")
+
+# 7. Backspace
+print("Rony \bSetiawan")
+
+# 8. Newline
+print("Baris pertama \nBaris Kedua")  # LF -> Line Feed
+print("Baris pertama \rBaris Kedua")  # CR -> Carriage Return
+# CRLF -> Line Feed Carriage Return > untukk windows
+print("Baris pertama \r\nBaris Kedua")
+
+# 9. String literal atau raw
+print("C:\\new Folder")
+print(r'C:\new Folder')
+
+# 10. Multiline String
+print("""""
+Hello My name is Rony
+Setiawan
+""")
+
+# 11. Multiline dan string literal
+print(r"""
+Hello World
+my website is
+https:\\github.com\new-file
+""")
+
+
+print("\n")
