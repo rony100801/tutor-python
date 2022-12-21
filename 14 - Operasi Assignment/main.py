f07b8ba84d09b5c469ec1c1973347ae6bb5b6608
rony100801 <setiawanrony603@gmail.com>
2022-12-21 04:45:12 +0000

    Belajar operator assignment

---
+++ 14 - Operasi Assignment/main.py
@@ -0,0 +1,62 @@
+# OPERATOR ASSIGNMENT
+print("\n")
+a = 5  # ini adalah assignment
+print("nilai a = ", a)
+
+a += 1  # artinya a = a + 1 (a = 5 + 1)
+print("nilai a += 1, nilai a menjadi = ", a)
+
+a -= 2  # artinya a = a - 2
+print("nilai a -= 2, nilai a menjadi = ", a)
+
+a *= 5  # artinya a = a * 2
+print("nilai a *= 5, nilai a menjadi = ", a)
+
+a /= 2  # artinya a = a / 2
+print("nilai a /= 2, nilai a menjadi = ", a)
+
+b = 10
+b %= 3  # artinya b = b % 3
+print("nilai b %= 3, nilai b menjadi = ", b)
+
+b = 10
+b //= 3  # artinya b = b // 3
+print("nilai b //= 3, nilai b menjadi = ", b)
+
+a = 5
+a **= 3
+print("nilai a **=3, nilai a menjadi", a)
+
+
+# OPERASI BITWISE
+# OR
+c = True
+print("\nnilai c =", c)
+c |= False
+print("nilai c |= False, nilai c menjadi", c)
+
+c = False
+c |= False
+print("nilai c |= False, nilai c menjadi", c)
+
+# AND
+c = True
+print("\nnilai c =", c)
+c &= False
+print("nilai c &= False, nilai c menjadi", c)
+
+c = True
+c &= True
+print("nilai c &= True, nilai c menjadi", c)
+
+# XOR
+c = True
+print("\nnilai c =", c)
+c ^= False
+print("nilai c ^= False, nilai c menjadi", c)
+
+c = True
+c ^= True
+print("nilai c ^= True, nilai c menjadi", c)
+
+print("\n")
